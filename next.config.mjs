c0ced50c49edb3b781c3a887fcc602a79191bfdf
bobby nicholson <b.nicholson@offensive-security.com>
2024-12-19 14:27:08 -0600

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'bn_resume';
-
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  basePath: '/bn_resume',
   images: {
     unoptimized: true,
-  },
-  assetPrefix: isProd ? `/${repoName}` : '',
-  basePath: isProd ? `/${repoName}` : '',
-  output: 'export',
-  trailingSlash: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    
-    // Update asset handling for fonts
-    config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/i,
-      type: 'asset/resource',
-    });
-
-    return config;
   }
 };
 
